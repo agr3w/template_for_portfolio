831e3d1ea0afd197e92e4192cbc1bf63cc076fe3
agr3w <suzanakampa.sk@gmail.com>
2024-10-03 16:22:14 -0300

    Suspense e lazy

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -7,12 +7,16 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import config from 'data/config.json';
-import HomePage from 'pages/HomePage/HomePage';
-import Portfolio from 'pages/PortfolioPage/Portfolio';
-import Contact from 'pages/ContactPage/Contact';
-import AboutPage from 'pages/AboutPage/AboutPage';
-import TetstimonialsPage from 'pages/TerstimonialsPage/TetstimonialsPage';
-import SkillsPage from 'pages/SkillsPage/SkillsPage';
+import { Box, CircularProgress } from '@mui/material';
+
+// Lazy load das páginas
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const Portfolio = lazy(() => import('pages/PortfolioPage/Portfolio'));
+const Contact = lazy(() => import('pages/ContactPage/Contact'));
+const AboutPage = lazy(() => import('pages/AboutPage/AboutPage'));
+const TetstimonialsPage = lazy(() => import('pages/TetstimonialsPage/TetstimonialsPage'));
+const SkillsPage = lazy(() => import('pages/SkillsPage/SkillsPage'));
+
 
 // Criando um tema personalizado com MUI
 const theme = createTheme({
@@ -38,15 +42,21 @@ const App = () => {
           title={site.title} 
           socialLinks={site.socialLinks} 
         />
-        <Routes>
-          <Route path="/" element={<HomePage config={config} />} />
-          <Route path="/portfolio" element={<Portfolio projects={config.projects} />} />
-          <Route path="/contato" element={<Contact contact={config.contact} />} />
-          <Route path="/sobre" element={<AboutPage config={config.site} />} />
-          <Route path="/testemunhos" element={<TetstimonialsPage testimonials={config.testimonials} />} />
-          <Route path="/habilidades" element={<SkillsPage skills={config.skills} />} />
-          {/* Adicione mais rotas conforme necessário */}
-        </Routes>
+         <Suspense fallback={
+          <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+            <CircularProgress />
+          </Box>
+        }>
+          <Routes>
+            <Route path="/" element={<HomePage config={config} />} />
+            <Route path="/portfolio" element={<Portfolio projects={config.projects} />} />
+            <Route path="/contato" element={<Contact contact={config.contact} />} />
+            <Route path="/sobre" element={<AboutPage config={config.site} />} />
+            <Route path="/testemunhos" element={<TetstimonialsPage testimonials={config.testimonials} />} />
+            <Route path="/habilidades" element={<SkillsPage skills={config.skills} />} />
+            {/* Adicione mais rotas conforme necessário */}
+          </Routes>
+        </Suspense>
         <Footer year={new Date().getFullYear()} author={site.author} />
       </Router>
     </ThemeProvider>
